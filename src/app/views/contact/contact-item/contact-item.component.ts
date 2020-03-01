cf0a51db0822793958b0501def72ed4692a5f030
juztinlazaro <juztinlazaro@gmail.com>
2020-03-01 09:43:15 +0800

    handle links

--- src/app/views/contact/contact-item/contact-item.component.ts
+++ src/app/views/contact/contact-item/contact-item.component.ts
@@ -1,14 +1,7 @@
 import { Component } from "@angular/core";
-import { Router, ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: "app-contact-item",
   templateUrl: "./contact-item.component.html"
 })
-export class ContactItemComponent {
-  constructor(private router: Router, private route: ActivatedRoute) {}
-
-  onRedirectEdit() {
-    this.router.navigate(["edit"]);
-  }
-}
+export class ContactItemComponent {}
