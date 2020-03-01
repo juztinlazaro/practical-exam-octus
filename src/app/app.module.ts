6b89a287fa01387f91d2297f7fde0b3c926740d9
juztinlazaro <juztinlazaro@gmail.com>
2020-03-01 13:37:55 +0800

    modal setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,8 +11,10 @@ import { SearchBarComponent } from "./views/contact/search-bar/search-bar.compon
 import { TestComponent } from "./views/test/test.component";
 import { ContactEditComponent } from "./views/contact-edit/contact-edit.component";
 import { ContactAddComponent } from "./views/contact-add/contact-add.component";
-import { HeaderComponent } from "./components/header/header.component";
 import { ContactDeleteComponent } from "./views/contact/contact-delete/contact-delete.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { ModalComponent } from "./components/modal/modal.component";
+
 import { PageTitleService } from "./common/service/pageTitle.service";
 
 @NgModule({
@@ -23,9 +25,10 @@ import { PageTitleService } from "./common/service/pageTitle.service";
     ContactEditComponent,
     ContactAddComponent,
     ContactDeleteComponent,
+    ContactComponent,
     ErrorComponent,
     HeaderComponent,
-    ContactComponent,
+    ModalComponent,
     SearchBarComponent,
     TestComponent
   ],
